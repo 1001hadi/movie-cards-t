f377fc50edc2f1da3ceb27664000f8b673be62b6
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-05-04 10:08:33 -0600

    homepage bug fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { MovieCard } from "../components/MovieCard";
 
-export const Home = () => {
+const Home = () => {
   const movies = [
     { id: 1, title: "OMD's favorite", release_date: "2019" },
     { id: 2, title: "BEN's favorite", release_date: "2015" },
@@ -11,10 +11,12 @@ export const Home = () => {
   return (
     <div className="home">
       <div className="movies-grid">
-        {movies.map((movie) => {
-          <MovieCard movie={movie} key={movie.id} />;
-        })}
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
       </div>
     </div>
   );
 };
+
+export default Home;
